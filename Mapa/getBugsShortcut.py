8ac79f9fc4f0f29e59a19f9f0370eb33988cc8d0
IsaacVRey <162612450+IsaacVRey@users.noreply.github.com>
2025-05-18 00:14:32 -0600

    Optimized the process to detect the bugs and deleted unnecesarry steps

--- Mapa/getBugsShortcut.py
+++ Mapa/getBugsShortcut.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 import pandas as pd
+import os
 
 dir = "./Mapa/datasets"
 
@@ -34,18 +35,23 @@ def Files():
     return files
 
 def Save_Files(files):
+    # Crea la carpeta si no existe
+    folder_path = f"{dir}/bugs"
+    os.makedirs(folder_path, exist_ok=True)
+
     # Extraer todos las calles con multidigit
     for file in files:
-        gdf = gpd.read_file(f"{dir}/multiDigs/multiDigs_SREETS_NAV_{file}.geojson")
-        if gdf.empty:
+        gdf = gpd.read_file(f"{dir}/STREETS_NAV/SREETS_NAV_{file}.geojson")
+        df = pd.read_csv(f'{dir}/POIs/POI_{file}.csv')
+        sts_with_N = df[df['POI_ST_SD'] == "N"]["LINK_ID"]
+        bugs = gdf[
+            (gdf["MULTIDIGIT"] == "Y") &
+            (gdf["link_id"].isin(sts_with_N))
+            ]
+        if bugs.empty:
             continue
-        df = pd.read_csv(f'{dir}/nonCardPOIs/nonCardPOI_POI_{file}.csv')
-        sts_with_N = df["LINK_ID"]
-        bugs = gdf[gdf["link_id"].isin(sts_with_N)]
-        bugs.to_file(f"{dir}/bugs/bugs_streets_{file}.geojson", driver="GeoJSON")
-
-
-
+        bugs.to_file(f"{dir}/bugs/BUGS_STREET_{file}.geojson", driver="GeoJSON")
+        
 
 files = Files()
 
