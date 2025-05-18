7507864857e85d7f7934240bd5976f315380474e
IsaacVRey <162612450+IsaacVRey@users.noreply.github.com>
2025-05-17 23:28:54 -0600

    Update getBugs.py
    It succesfully classifies the POI that are not on the left or right of Multiply Digitised roads

---
+++ Mapa/getBugs.py
@@ -0,0 +1,52 @@
+import geopandas as gpd
+import pandas as pd
+
+dir = "./Mapa/datasets"
+
+# Leer el GeoJSON
+def Files():
+    files = [
+        "4815075",
+        "4815078",
+        "4815079",
+        "4815081",
+        "4815083",
+
+        "4815084",
+        "4815085",
+        "4815086",
+        "4815087",
+        "4815090",
+
+        "4815091",
+        "4815096",
+        "4815097",
+        "4815098",
+        "4815099",
+        
+        "4815425",
+        "4815428",
+        "4815429",
+        "4815440",
+        "4815441"
+    ]
+
+    return files
+
+def Save_Files(files):
+    # Extraer todos las calles con multidigit
+    for file in files:
+        gdf = gpd.read_file(f"{dir}/multiDigs/multiDigs_SREETS_NAV_{file}.geojson")
+        if gdf.empty:
+            continue
+        df = pd.read_csv(f'{dir}/nonCardPOIs/nonCardPOI_POI_{file}.csv')
+        sts_with_N = df["LINK_ID"]
+        bugs = gdf[gdf["link_id"].isin(sts_with_N)]
+        bugs.to_file(f"{dir}/bugs/bugs_streets_{file}.geojson", driver="GeoJSON")
+
+
+
+
+files = Files()
+
+Save_Files(files)
